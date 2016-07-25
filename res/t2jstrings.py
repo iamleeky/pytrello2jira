b6a27d1d11548e9ad76135fde6b155179945a9d0
Eric Lee <iamleeky@gmail.com>
2016-07-26 02:54:11 +0900

    parse trello json file and map to jira fields

--- res/t2jstrings.py
+++ res/t2jstrings.py
@@ -12,6 +12,7 @@ conf_encoding = 'utf8'
 
 section_jira = 'jira'
 key_jira_url = 'url'
+key_jira_project = 'project'
 key_jira_username = 'username'
 key_jira_password = 'password'
 # -------------------------------------------
@@ -37,4 +38,50 @@ logging_conf_path = 'log/t2jlogging.json'
 # exceptions
 # -------------------------------------------
 exc_config_read_error = 'error occurs while reading configuration'
+# -------------------------------------------
+
+# path
+# -------------------------------------------
+dir_temp = './temp'
+dir_error = dir_temp + '/' + 'error'
+dir_extract = dir_temp + '/' + 'extract'
+file_extract = 'extract.json'
+# -------------------------------------------
+
+# information
+# -------------------------------------------
+info_create_issue_error = 'There is a failed item. Saved to %s.' % dir_error
+info_extract_done = 'All fields has been extracted. Saved to %s.' % dir_extract
+# -------------------------------------------
+
+# jira
+# -------------------------------------------
+order = 'order'
+jira_field_project = 'project'
+jira_field_project_id = 'id'
+jira_field_summary = 'summary'
+jira_field_description = 'description'
+jira_field_issuetype = 'issuetype'
+jira_field_issuetype_name = 'name'
+jira_field_issuetype_bug = 'Bug'
+jira_field_issuetype_task = 'Task'
+jira_field_labels = 'labels'
+jira_field_attachs = 'attachments'
+# -------------------------------------------
+
+# trello
+# -------------------------------------------
+trel_field_boardname = 'name'
+trel_field_boardurl = 'url'
+trel_field_boardcards = 'cards'
+trel_field_boardlists = 'lists'
+trel_field_listid = 'id'
+trel_field_listname = 'name'
+trel_field_cardname = 'name'
+trel_field_carddesc = 'desc'
+trel_field_cardurl = 'url'
+trel_field_cardclosed = 'closed'
+trel_field_cardidlist = 'idList'
+trel_field_cardattachs = 'attachments'
+trel_field_attachurl = 'url'
 # -------------------------------------------
--- t2j/trello2jira.py
+++ t2j/trello2jira.py
@@ -5,20 +5,26 @@
 This module implements Trello to JIRA importer.
 """
 
+import os
 import argparse
+import json
+from jira import JIRA
 import res.t2jstrings as strings
 import log.mylogging as mylogging
 import exc.myexceptions as exceptions
 
 
 class Trello2Jira(object):
-    _logger = None
-    _args = {}
-    _config = None
-    _username = ''
-    _password = ''
-    _url = ''
-    _file_list = []
+    _logger = None  # logger
+    _args = {}  # command line arguments
+    _config = None  # user configuration
+    _username = ''  # jira user name
+    _password = ''  # jira user password
+    _url = ''  # jira project url
+    _project = ''  # jira project key
+    _file_list = []  # json files exported from trello
+    _field_list = []  # fields to be used to create jira issues
+    _issue_list = []  # finally created jira issues
 
     def __init__(self, config):
         if not config:
@@ -53,7 +59,82 @@ class Trello2Jira(object):
         """
         get jira project url
         """
-        self._username = self._config.get(strings.section_jira, strings.key_jira_url)
+        self._url = self._config.get(strings.section_jira, strings.key_jira_url)
+
+    def _read_project(self):
+        """
+        get jira project key
+        """
+        self._project = self._config.get(strings.section_jira, strings.key_jira_project)
+
+    def _create_issues(self):
+        """
+        create jira issues with extracted fields
+
+        issue = jira.create_issue(fields=issue_dict)
+        issue.update(labels=['AAA', 'BBB'])
+        jira.add_attachment(issue=issue, attachment=attachment, filename='content.txt')
+        """
+        jira = JIRA(server=self._url, basic_auth=(self._username, self._password))
+
+        for field in self._field_list:
+            try:
+                # issue = jira.create_issue(fields=field)
+                issue = jira.create_issue(fields={'summary': 'test summary'})
+                self._issue_list.append(issue)
+            except Exception as err:
+                self._logger.error(strings.info_create_issue_error)
+                if not os.path.exists(strings.dir_error):
+                    os.makedirs(strings.dir_error)
+                fp = open('%s/%d.json' % (strings.dir_error, field[strings.order]), 'w')
+                json.dump(field, fp, indent=2)
+
+    def _extract_fields(self):
+        """
+        extract issue fields from exported trello json files
+
+        issue_dict = {
+            'project': {'id': 123},
+            'summary': 'New issue from jira-python',
+            'description': 'Look into this one',
+            'issuetype': {'name': 'Bug'},
+        }
+        """
+        for file in self._file_list:
+            fp = open(file, 'r', encoding='utf-8')
+            board = json.load(fp)
+            fp.close()
+
+            boardlists = dict()
+            for lst in board[strings.trel_field_boardlists]:
+                boardlists[lst[strings.trel_field_listid]] = lst[strings.trel_field_listname]
+
+            for order, card in enumerate(board[strings.trel_field_boardcards]):
+                if card[strings.trel_field_cardclosed]:
+                    continue
+
+                field = dict()
+                field[strings.order] = order
+                field[strings.jira_field_project] = {strings.jira_field_project_id: self._project}
+                field[strings.jira_field_summary] = card[strings.trel_field_cardname]
+                field[strings.jira_field_description] = card[strings.trel_field_carddesc]
+                field[strings.jira_field_issuetype] = \
+                    {strings.jira_field_issuetype_name: strings.jira_field_issuetype_task}
+                field[strings.jira_field_labels] = [board[strings.trel_field_boardname],
+                                                    board[strings.trel_field_boardurl],
+                                                    card[strings.trel_field_cardurl],
+                                                    boardlists[card[strings.trel_field_cardidlist]]]
+                field[strings.jira_field_attachs] = \
+                    [attach[strings.trel_field_attachurl] for attach in card[strings.trel_field_cardattachs]]
+
+                self._field_list.append(field)
+
+        # save to file
+        self._logger.info(strings.info_extract_done)
+        if not os.path.exists(strings.dir_extract):
+            os.makedirs(strings.dir_extract)
+        fp = open('%s/%s' % (strings.dir_extract, strings.file_extract), 'w')
+        json.dump(self._field_list, fp, indent=2)
 
     def run(self):
         """
@@ -63,6 +144,10 @@ class Trello2Jira(object):
         self._parse_file_list()
         self._read_auth()
         self._read_url()
+        self._read_project()
 
         self._logger.debug(strings.dbg_src_files_info + ' '.join(self._file_list))
         self._logger.debug(strings.dbg_user_info + self._username)
+
+        self._extract_fields()
+        self._create_issues()
